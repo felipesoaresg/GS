cc16e12fecf0e2e7727784dfa5dc2536e96b7cc1
felipesoaresg <felisoaresg@gmail.com>
2025-11-20 20:51:30 -0300

    add type from users

---
+++ types/usuario.ts
@@ -0,0 +1,8 @@
+export interface Usuario {
+  IDUSUARIO: number;
+  NOME_USUARIO: string;
+  EMAIL_USUARIO: string;
+  SENHA_USUARIO: string;
+  CARGO_USUARIO?: string;
+  ACESSIBILIDADE?: string;
+}
