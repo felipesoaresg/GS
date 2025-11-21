e31d5adcb0d8f16aff921fcafccd4e0af1942143
felipesoaresg <felisoaresg@gmail.com>
2025-11-21 00:04:49 -0300

    add tarefas api

---
+++ api/tarefas.ts
@@ -0,0 +1,20 @@
+import { Tarefa } from "@/types/tarefas";
+import api from "./api";
+
+export async function listarTarefasPorUsuario(idusuario: number): Promise<Tarefa[]> {
+  const response = await api.get<Tarefa[]>("/tarefas");
+  return response.data.filter((t) => t.idusuario === idusuario);
+}
+
+export async function criarTarefa(tarefa: {
+  idusuario: number;
+  titulo_tarefa: string;
+  descricao_tarefa: string;
+  status_tarefa: string;
+}): Promise<void> {
+  await api.post("/tarefas", tarefa);
+}
+
+export async function excluirTarefa(idtarefa: number): Promise<void> {
+  await api.delete(`/tarefas/${idtarefa}`);
+}
