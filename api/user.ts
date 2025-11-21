e9bdcb91b91eb14df7208b0f26b346b4ab040484
felipesoaresg <felisoaresg@gmail.com>
2025-11-20 23:51:58 -0300

    update user for api

--- api/user.ts
+++ api/user.ts
@@ -3,12 +3,11 @@ import api from "./api";
 
 export async function autenticar(email: string, senha: string): Promise<Usuario | null> {
   const response = await api.get<Usuario[]>("/usuarios");
-  console.log("Resposta da API:", response.data);
 
   const usuarios = response.data;
 
   const usuario = usuarios.find(
-    (u) => u.EMAIL_USUARIO === email && u.SENHA_USUARIO === senha
+    (u) => u.email_usuario === email && u.senha_usuario === senha
   );
 
   return usuario || null;
